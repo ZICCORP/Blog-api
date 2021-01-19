b03c040e989f40a5f7b76027828ba50d013b82ff
ZICCORP <frankchuka250@gmail.com>
2021-01-19 17:33:17 +0100

    Added custom permission to detail view, so only an author of a blog post can perform 'risky' actions e.g deletion

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 
@@ -10,5 +11,7 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+   
